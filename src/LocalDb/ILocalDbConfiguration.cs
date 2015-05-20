47d95e02125980dfc62a00c0f1476f4815c74c51
Khalid Abuhakmeh <khalid.abuhakmeh@ritterim.com>
2015-05-20 08:52:37 -0400

    Added DB suffix generator to LocalDb
    Upgraded LocalDb library to 0.2.0 to support datbase suffix feature
    that will allow sandbox users to generate the suffix appeneded
    to the database name.
    
    The new default, the suffix is now {Guid}_{DateTime.Now.Ticks}. This
    will garauntee uniqueness of database names, while still allowing users
    to narrow down a database to a particular time (if necessary).
    
    Bumped Sandbox version to 0.2.0

--- src/LocalDb/ILocalDbConfiguration.cs
+++ src/LocalDb/ILocalDbConfiguration.cs
@@ -1,9 +1,12 @@
-﻿namespace RimDev.Sandbox.LocalDb
+﻿using System;
+
+namespace RimDev.Sandbox.LocalDb
 {
     public interface ILocalDbConfiguration
     {
         ILocalDbConfiguration WithVersion(string version);
         ILocalDbConfiguration WithDatabaseName(string databaseName);
         ILocalDbConfiguration WithDatabasePrefix(string databasePrefix);
+        ILocalDbConfiguration WithDatabaseSuffix(Func<string> databaseSuffixGenerator);
     }
 }
